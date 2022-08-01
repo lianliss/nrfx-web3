29c95fb0dabf110b4baea5f902d24658420c697a
Danil Sakhinov <ldevil2209@ya.ru>
2022-08-01 15:46:59 +0800

    Turn back Telegram

--- controllers/run.js
+++ controllers/run.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 const logger = require('../utils/logger');
-//const telegram = require('../services/telegram');
+const telegram = require('../services/telegram');
 const web3Service = require('../services/web3');
 const User = require('../models/user');
 const {setUserWallet, getUserWallets} = require('../models/db/web3-wallets');
@@ -16,54 +16,12 @@ const run = async () => {
         logger.info('Server started');
         require('../services/stream');
 
-        // const db = require('../services/mysql');
-        // const wallets = {
-        //     '0x17c000fcbfe5cf32889b299fff076f271e800e61': {
-        //         version: 3,
-        //         id: '71e3cabd-4102-49b7-851f-e5e7bc7c271c',
-        //         address: '17c000fcbfe5cf32889b299fff076f271e800e61',
-        //         crypto: {
-        //             ciphertext: '777671eb36ab6c02ac6e430211b367c3e8ec87303c52877f2ae2a31469f8969f',
-        //             cipherparams: { iv: 'e65af604cf08151f2488de261a15ca71' },
-        //             cipher: 'aes-128-ctr',
-        //             kdf: 'scrypt',
-        //             kdfparams: {
-        //                 dklen: 32,
-        //                 salt: 'e8e057149d0e6823e2066ba4915b1b89504a8ac1b1b59d608a0bd114c30b1a61',
-        //                 n: 8192,
-        //                 r: 8,
-        //                 p: 1
-        //             },
-        //             mac: '8182c375e1e7f8006aaf4dfd4f7e6dc2c766384200b510b0c820ec58bdc94996'
-        //         }
-        //     },
-        // };
-        //
-        // Object.keys(wallets).map(address => {
-        //     const encryption = JSON.stringify(wallets[address]);
-        //     const record = db.query(`
-        //         SELECT *
-        //         FROM web3_wallets
-        //         WHERE address = '${address}';
-        //     `).then(data => {
-        //         logger.debug('Found wallets', address, data);
-        //         data.map(row => {
-        //             setUserWallet({
-        //                 userID: row.user_id,
-        //                 address,
-        //                 encryption,
-        //             });
-        //         })
-        //     }).catch(error => {
-        //         logger.debug(`Can't load ${address} data`);
-        //     });
-        // });
-	
+        telegram.log('Started');
 
         // Run jobs
         const jobs = require('../services/jobs');
     } catch(error) {
-        //telegram.log('Run error. Waiting...');
+        telegram.log('Run error. Waiting...');
         logger.error('Start error', error);
 
         setTimeout(() => {
--- services/telegram.js
+++ services/telegram.js
@@ -5,189 +5,200 @@ const {exec} = require('child_process');
 const isLocal = process.env.NODE_ENV === 'local';
 const _ = require('lodash');
 
-const telegram = new Telegraf(config.telegram.token);
+let telegram;
 
-const serverKeyboard = Markup.inlineKeyboard([
+if (isLocal) {
+  telegram = {
+    log: message => logger.debug('[Telegram local]', message),
+  }
+} else {
+
+  telegram = new Telegraf(config.telegram.token);
+
+  const serverKeyboard = Markup.inlineKeyboard([
     Markup.button.callback('Restart', 'restart'),
     Markup.button.callback('Pull', 'pull'),
     Markup.button.callback('Update', 'update'),
     Markup.button.callback('Rebuild', 'build'),
     Markup.button.callback('Update Frond', 'updatefront'),
-]);
+  ]);
 
-const execOptions = {
+  const execOptions = {
     maxBuffer: 1024 * 1024 * 10,
-};
+  };
 
-const frontExecOptions = {
+  const frontExecOptions = {
     ...execOptions,
     cwd: '/root/bot-ui',
-};
+  };
 
-const startCommand = ctx => {
+  const startCommand = ctx => {
     ctx.reply(`Test is ok ${Date.now()}`);
     ctx.telegram.sendMessage(ctx.message.chat.id, `id is ${ctx.message.chat.id}`);
-};
-const restartCommand = ctx => {
+  };
+  const restartCommand = ctx => {
     if (ctx.chat.id === config.telegram.chatId) {
-        ctx.reply(`Restarting...`);
-        exec('pm2 restart ai', (err, stdout, stderr) => {
-            if (err) {
-                logger.error('[telegram][restartCommand]', err);
-                ctx.reply(`Can't execute restartCommand`);
-            }
-        });
+      ctx.reply(`Restarting...`);
+      exec('pm2 restart ai', (err, stdout, stderr) => {
+        if (err) {
+          logger.error('[telegram][restartCommand]', err);
+          ctx.reply(`Can't execute restartCommand`);
+        }
+      });
     } else {
-        ctx.reply(`You have no permission to restart`);
+      ctx.reply(`You have no permission to restart`);
     }
-};
-const pullCommand = ctx => {
+  };
+  const pullCommand = ctx => {
     if (ctx.chat.id === config.telegram.chatId) {
-        ctx.reply(`Git pull...`);
-        exec('git pull', (err, stdout, stderr) => {
+      ctx.reply(`Git pull...`);
+      exec('git pull', (err, stdout, stderr) => {
+        if (err) {
+          logger.error('[telegram][pullCommand]', err);
+          ctx.reply(`Can't execute pullCommand`);
+        } else {
+          ctx.reply(stdout);
+          restartCommand(ctx);
+        }
+      });
+    } else {
+      ctx.reply(`You have no permission`);
+    }
+  };
+  const updateCommand = ctx => {
+    if (ctx.chat.id === config.telegram.chatId) {
+      ctx.reply(`Installing nodes...`);
+      exec('git pull', (err, stdout, stderr) => {
+        if (err) {
+          logger.error('[telegram][pullCommand]', err);
+          ctx.reply(`Can't execute pullCommand`);
+        } else {
+          ctx.reply(stdout);
+          exec('npm run reset', execOptions, (err, stdout, stderr) => {
             if (err) {
-                logger.error('[telegram][pullCommand]', err);
-                ctx.reply(`Can't execute pullCommand`);
+              logger.error('[telegram][updateCommand]', err);
+              ctx.reply(`Can't execute updateCommand`);
             } else {
-                ctx.reply(stdout);
-                restartCommand(ctx);
+              ctx.reply(`node_modules updated`);
+              restartCommand(ctx);
             }
-        });
+          });
+        }
+      });
     } else {
-        ctx.reply(`You have no permission`);
+      ctx.reply(`You have no permission`);
     }
-};
-const updateCommand = ctx => {
+  };
+  const updateFrontCommand = ctx => {
     if (ctx.chat.id === config.telegram.chatId) {
-        ctx.reply(`Installing nodes...`);
-        exec('git pull', (err, stdout, stderr) => {
+      ctx.reply(`Updating front...`);
+      exec('git pull', frontExecOptions, (err, stdout, stderr) => {
+        if (err) {
+          logger.error('[telegram][updateFrontCommand]', err);
+          ctx.reply(`Can't execute updateFrontCommand`);
+        } else {
+          ctx.reply(stdout);
+          exec('npm run reset', frontExecOptions, (err, stdout, stderr) => {
             if (err) {
-                logger.error('[telegram][pullCommand]', err);
-                ctx.reply(`Can't execute pullCommand`);
+              logger.error('[telegram][updateFrontCommand]', err);
+              ctx.reply(`Can't execute updateFrontCommand [reset]`);
             } else {
-                ctx.reply(stdout);
-                exec('npm run reset', execOptions, (err, stdout, stderr) => {
-                    if (err) {
-                        logger.error('[telegram][updateCommand]', err);
-                        ctx.reply(`Can't execute updateCommand`);
-                    } else {
-                        ctx.reply(`node_modules updated`);
-                        restartCommand(ctx);
-                    }
-                });
+              ctx.reply('Front modules updated.');
+              exec('npm run build', frontExecOptions, (err, stdout, stderr) => {
+                if (err) {
+                  logger.error('[telegram][updateFrontCommand]', err);
+                  ctx.reply(`Can't execute updateFrontCommand [build]`);
+                } else {
+                  ctx.reply('Scripts rebulded.');
+                }
+              });
             }
-        });
+          })
+        }
+      });
     } else {
-        ctx.reply(`You have no permission`);
+      ctx.reply(`You have no permission`);
     }
-};
-const updateFrontCommand = ctx => {
+  };
+  const buildFrontCommand = ctx => {
     if (ctx.chat.id === config.telegram.chatId) {
-        ctx.reply(`Updating front...`);
-        exec('git pull', frontExecOptions, (err, stdout, stderr) => {
+      ctx.reply(`Rebuilding front...`);
+      exec('git pull', frontExecOptions, (err, stdout, stderr) => {
+        if (err) {
+          logger.error('[telegram][buildFrontCommand]', err);
+          ctx.reply(`Can't execute buildFrontCommand`);
+        } else {
+          ctx.reply(stdout);
+          exec('npm run build', frontExecOptions, (err, stdout, stderr) => {
             if (err) {
-                logger.error('[telegram][updateFrontCommand]', err);
-                ctx.reply(`Can't execute updateFrontCommand`);
+              logger.error('[telegram][buildFrontCommand]', err);
+              ctx.reply(`Can't execute buildFrontCommand`);
             } else {
-                ctx.reply(stdout);
-                exec('npm run reset', frontExecOptions, (err, stdout, stderr) => {
-                    if (err) {
-                        logger.error('[telegram][updateFrontCommand]', err);
-                        ctx.reply(`Can't execute updateFrontCommand [reset]`);
-                    } else {
-                        ctx.reply('Front modules updated.');
-                        exec('npm run build', frontExecOptions, (err, stdout, stderr) => {
-                            if (err) {
-                                logger.error('[telegram][updateFrontCommand]', err);
-                                ctx.reply(`Can't execute updateFrontCommand [build]`);
-                            } else {
-                                ctx.reply('Scripts rebulded.');
-                            }
-                        });
-                    }
-                })
+              ctx.reply('Front scripts rebuilded.');
             }
-        });
+          })
+        }
+      });
     } else {
-        ctx.reply(`You have no permission`);
+      ctx.reply(`You have no permission`);
     }
-};
-const buildFrontCommand = ctx => {
-    if (ctx.chat.id === config.telegram.chatId) {
-        ctx.reply(`Rebuilding front...`);
-        exec('git pull', frontExecOptions, (err, stdout, stderr) => {
-            if (err) {
-                logger.error('[telegram][buildFrontCommand]', err);
-                ctx.reply(`Can't execute buildFrontCommand`);
-            } else {
-                ctx.reply(stdout);
-                exec('npm run build', frontExecOptions, (err, stdout, stderr) => {
-                    if (err) {
-                        logger.error('[telegram][buildFrontCommand]', err);
-                        ctx.reply(`Can't execute buildFrontCommand`);
-                    } else {
-                        ctx.reply('Front scripts rebuilded.');
-                    }
-                })
-            }
-        });
-    } else {
-        ctx.reply(`You have no permission`);
-    }
-};
+  };
 
-telegram.command('start', startCommand);
-telegram.command('restart', restartCommand);
-telegram.command('pull', pullCommand);
-telegram.command('update', updateCommand);
-telegram.command('build', buildFrontCommand);
-telegram.command('updatefront', updateFrontCommand);
+  telegram.command('start', startCommand);
+  telegram.command('restart', restartCommand);
+  telegram.command('pull', pullCommand);
+  telegram.command('update', updateCommand);
+  telegram.command('build', buildFrontCommand);
+  telegram.command('updatefront', updateFrontCommand);
 
-telegram.action('restart', restartCommand);
-telegram.action('pull', pullCommand);
-telegram.action('update', updateCommand);
-telegram.action('build', buildFrontCommand);
-telegram.action('updatefront', updateFrontCommand);
+  telegram.action('restart', restartCommand);
+  telegram.action('pull', pullCommand);
+  telegram.action('update', updateCommand);
+  telegram.action('build', buildFrontCommand);
+  telegram.action('updatefront', updateFrontCommand);
 
-telegram.help((ctx) => {
+  telegram.help((ctx) => {
     if (ctx.message.chat.id === config.telegram.chatId) {
-        ctx.telegram.sendMessage(
-            ctx.from.id,
-            'There is a commands',
-            serverKeyboard)
+      ctx.telegram.sendMessage(
+        ctx.from.id,
+        'There is a commands',
+        serverKeyboard)
     } else {
-        ctx.reply(`You have no permission`);
+      ctx.reply(`You have no permission`);
     }
-});
+  });
 
-telegram.log = (message, isNotify = false) => {
+  telegram.log = (message, isNotify = false) => {
     if (isLocal) return;
     telegram.telegram.sendMessage(
-        config.telegram.chatId,
-        message,
-        {
-            disable_notification: isNotify,
-        }
-        );
-};
+      config.telegram.chatId,
+      message,
+      {
+        disable_notification: isNotify,
+      }
+    );
+  };
+}
+
+
 
 ['SIGINT', 'SIGTERM', 'SIGHUP'].map(reason => {
-    process.once(reason, reason => {
-        logger.info('Process stop', reason);
-        if (!isLocal) {
-            telegram.log(`Server stopped ${reason}`);
-            telegram.stop(reason);
-        }
-        setTimeout(() => {
-            process.exit();
-        }, 10)
-    });
+  process.once(reason, reason => {
+    logger.info('Process stop', reason);
+    if (!isLocal) {
+      telegram.log(`Server stopped ${reason}`);
+      telegram.stop(reason);
+    }
+    setTimeout(() => {
+      process.exit();
+    }, 10)
+  });
 });
 
 process.once('unhandledRejection', (promise, reason) => {
-    const message = _.get(reason, 'message', 'No message');
-    logger.error('unhandledRejection', reason);
-    telegram.log(`unhandledRejection reason: ${reason} ${message}`, true);
+  const message = _.get(reason, 'message', 'No message');
+  logger.error('unhandledRejection', reason);
+  telegram.log(`unhandledRejection reason: ${reason} ${message}`, true);
 });
 
 module.exports = telegram;

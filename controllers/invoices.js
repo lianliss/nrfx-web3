f7a48cc1f316b9c32d5142606792270040d72efd
Danil Sakhinov <ldevil2209@ya.ru>
2022-09-11 05:44:07 +0800

    Fix sending invoice message

--- controllers/invoices.js
+++ controllers/invoices.js
@@ -10,112 +10,116 @@ const topupMethods = require('../const/topupMethods');
 const invoiceLogic = require('../logic/invoice');
 
 const addInvoice = (req, res) => {
-    (async () => {
-        try {
-            const {accountAddress} = res.locals;
-            const amount = Number(_.get(req, 'query.amount', 0));
-            const currency = _.get(req, 'query.currency', undefined);
-            const phone = _.get(req, 'query.phone');
-            const name = _.get(req, 'query.name');
-            const lastName = _.get(req, 'query.lastName');
+  (async () => {
+    try {
+      const {accountAddress} = res.locals;
+      const amount = Number(_.get(req, 'query.amount', 0));
+      const currency = _.get(req, 'query.currency', undefined);
+      const phone = _.get(req, 'query.phone');
+      const name = _.get(req, 'query.name');
+      const lastName = _.get(req, 'query.lastName');
 
+      const invoice = await db.addInvoice(amount, currency, accountAddress, phone, name, lastName);
 
-            const result = await db.addInvoice(amount, currency, accountAddress, phone, name, lastName);
+      const admins = await db.getAdminsWithTelegram();
+      admins.map(user => {
+        telegram.sendInvoice(user, invoice);
+      });
 
-            res.status(200).json(result);
-        } catch (error) {
-            logger.error('[invoiceController][addInvoice]', error);
-            res.status(500).json({
-                name: error.name,
-                message: error.message,
-            });
-        }
-    })();
+      res.status(200).json(invoice);
+    } catch (error) {
+      logger.error('[invoiceController][addInvoice]', error);
+      res.status(500).json({
+        name: error.name,
+        message: error.message,
+      });
+    }
+  })();
 };
 
 const getInvoice = (req, res) => {
-    (async () => {
-        try {
-            const {accountAddress} = res.locals;
-            const result = await db.getInvoice(accountAddress);
+  (async () => {
+    try {
+      const {accountAddress} = res.locals;
+      const result = await db.getInvoice(accountAddress);
 
-            res.status(200).json(result[0]);
-        } catch (error) {
-            logger.error('[invoiceController][getInvoice]', error);
-            res.status(500).json({
-                name: error.name,
-                message: error.message,
-            });
-        }
-    })();
+      res.status(200).json(result[0]);
+    } catch (error) {
+      logger.error('[invoiceController][getInvoice]', error);
+      res.status(500).json({
+        name: error.name,
+        message: error.message,
+      });
+    }
+  })();
 };
 
 const cancelInvoice = (req, res) => {
-    (async () => {
-        try {
-            const {accountAddress} = res.locals;
-            const invoices = await db.getInvoice(accountAddress);
-            if (!invoices.length) throw new Error('No invoices for this address');
+  (async () => {
+    try {
+      const {accountAddress} = res.locals;
+      const invoices = await db.getInvoice(accountAddress);
+      if (!invoices.length) throw new Error('No invoices for this address');
 
-            const result = await db.cancelInvoice(invoices[0].id);
+      const result = await db.cancelInvoice(invoices[0].id);
 
-            res.status(200).json(result[0]);
-        } catch (error) {
-            logger.error('[invoiceController][cancelInvoice]', error);
-            res.status(500).json({
-                name: error.name,
-                message: error.message,
-            });
-        }
-    })();
+      res.status(200).json(result[0]);
+    } catch (error) {
+      logger.error('[invoiceController][cancelInvoice]', error);
+      res.status(500).json({
+        name: error.name,
+        message: error.message,
+      });
+    }
+  })();
 };
 
 const reviewInvoice = (req, res) => {
-    (async () => {
-        try {
-            const {accountAddress} = res.locals;
-            const invoices = await db.getInvoice(accountAddress);
-            if (!invoices.length) throw new Error('No invoices for this address');
-            const invoice = invoices[0];
-            if (invoice.status !== 'wait_for_pay' && invoice.status !== 'wait_for_review') {
-                throw new Error('Invoice is unavailable');
-            }
+  (async () => {
+    try {
+      const {accountAddress} = res.locals;
+      const invoices = await db.getInvoice(accountAddress);
+      if (!invoices.length) throw new Error('No invoices for this address');
+      const invoice = invoices[0];
+      if (invoice.status !== 'wait_for_pay' && invoice.status !== 'wait_for_review') {
+        throw new Error('Invoice is unavailable');
+      }
 
-            const result = await db.reviewInvoice(invoice.id);
+      const result = await db.reviewInvoice(invoice.id);
 
-            const admins = await db.getAdminsWithTelegram();
-            admins.map(user => {
-                telegram.sendInvoice(user, invoice);
-            });
+      const admins = await db.getAdminsWithTelegram();
+      admins.map(user => {
+        telegram.sendInvoice(user, invoice);
+      });
 
-            res.status(200).json(result[0]);
-        } catch (error) {
-            logger.error('[invoiceController][reviewInvoice]', error);
-            res.status(500).json({
-                name: error.name,
-                message: error.message,
-            });
-        }
-    })();
+      res.status(200).json(result[0]);
+    } catch (error) {
+      logger.error('[invoiceController][reviewInvoice]', error);
+      res.status(500).json({
+        name: error.name,
+        message: error.message,
+      });
+    }
+  })();
 };
 
 const confirmInvoice = (req, res) => {
-    (async () => {
-        try {
-            const id = Number(_.get(req, 'query.id'));
-            const amount = Number(_.get(req, 'query.amount'));
+  (async () => {
+    try {
+      const id = Number(_.get(req, 'query.id'));
+      const amount = Number(_.get(req, 'query.amount'));
 
-            const receipt = await topupLogic.approveInvoice(id, amount);
+      const receipt = await topupLogic.approveInvoice(id, amount);
 
-            res.status(200).json(receipt);
-        } catch (error) {
-            logger.error('[invoiceController][confirmInvoice]', error);
-            res.status(500).json({
-                name: error.name,
-                message: error.message,
-            });
-        }
-    })();
+      res.status(200).json(receipt);
+    } catch (error) {
+      logger.error('[invoiceController][confirmInvoice]', error);
+      res.status(500).json({
+        name: error.name,
+        message: error.message,
+      });
+    }
+  })();
 };
 
 const getPDF = (req, res) => {
@@ -141,12 +145,11 @@ const getPDF = (req, res) => {
 };
 
 
-
 module.exports = {
-    addInvoice,
-    getInvoice,
-    cancelInvoice,
-    reviewInvoice,
-    confirmInvoice,
+  addInvoice,
+  getInvoice,
+  cancelInvoice,
+  reviewInvoice,
+  confirmInvoice,
   getPDF,
 };

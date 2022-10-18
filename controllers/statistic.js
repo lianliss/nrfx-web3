bf4a195407ab1633f7f4f576355f9d98d43ab65a
Danil Sakhinov <ldevil2209@ya.ru>
2022-10-18 16:43:11 +0800

    Fix statistic

--- controllers/statistic.js
+++ controllers/statistic.js
@@ -5,142 +5,81 @@ const errors = require('../models/error');
 const xl = require('excel4node');
 
 /**
- * Returns money operations statistic
+ * Returns money transactions statistic
  * @param req
  * @param res
  */
-const getOperations = (req, res) => {
-    (async () => {
-        try {
-            const {user} = res.locals;
-            if (!user.isAdmin) throw new errors.PermissionDeniedError();
-
-            // Get DB data
-            const data = await Promise.all([
-                db.getBalancesHistory(),
-                db.getBankCardOperations(),
-            ]);
-
-            // Create a book
-            const wb = new xl.Workbook({
-                defaultFont: {
-                    size: 12,
-                    name: 'Calibri',
-                },
-                author: 'Narfex',
-            });
-
-            const operations = wb.addWorksheet('Bank Operations');
-            operations.cell(1, 1).string('ID');
-            operations.cell(1, 2).string('Date');
-            operations.cell(1, 3).string('ManagerID');
-            operations.cell(1, 4).string('Manager');
-            operations.cell(1, 5).string('Card');
-            operations.cell(1, 6).string('Fiat');
-            operations.cell(1, 7).string('Amount');
-            operations.cell(1, 8).string('Fee');
-            operations.cell(1, 9).string('UserID');
-            operations.cell(1, 10).string('First Name');
-            operations.cell(1, 11).string('Last Name');
-            operations.cell(1, 12).string('Login');
-            operations.cell(1, 13).string('Email');
-
-            const swaps = wb.addWorksheet('Swaps');
-            swaps.cell(1, 1).string('ID');
-            swaps.cell(1, 2).string('Date');
-            swaps.cell(1, 3).string('Amount');
-            swaps.cell(1, 4).string('From');
-            swaps.cell(1, 5).string('To');
-            swaps.cell(1, 6).string('Tokens');
-            swaps.cell(1, 7).string('Price');
-            swaps.cell(1, 8).string('Commission');
-            swaps.cell(1, 9).string('UserID');
-            swaps.cell(1, 10).string('First Name');
-            swaps.cell(1, 11).string('Last Name');
-            swaps.cell(1, 12).string('Login');
-            swaps.cell(1, 13).string('Email');
-
-            // Process bank cards operations
-            data[1].map((row, index) => {
-                const i = index + 2;
-                let extra = {};
-                try {
-                    extra = JSON.parse(row.extra);
-                } catch (error) {
-                    logger.warn("[getOperations] Can't parse Swaps", row.id);
-                }
-
-                swaps.cell(i, 1)
-                    .number(row.id);
-                swaps.cell(i, 2)
-                    .date(new Date(row.created_at_timestamp * 1000));
-                swaps.cell(i, 3)
-                    .number(row.amount);
-                swaps.cell(i, 4)
-                    .string(extra.from_currency || '');
-                swaps.cell(i, 5)
-                    .string(extra.to_currency || '');
-                swaps.cell(i, 6)
-                    .number(extra.crypto_amount || 0);
-                swaps.cell(i, 7)
-                    .number(extra.price || 0);
-                swaps.cell(i, 8)
-                    .number(extra.commission || 0);
-                swaps.cell(i, 9)
-                    .number(row.user_id);
-                swaps.cell(i, 10)
-                    .string(row.first_name);
-                swaps.cell(i, 11)
-                    .string(row.last_name);
-                swaps.cell(i, 12)
-                    .string(row.login);
-                swaps.cell(i, 13)
-                    .string(row.email);
-            });
-
-            // Process swaps
-            data[0].map((row, index) => {
-                const i = index + 2;
-
-                operations.cell(i, 1)
-                    .number(row.id);
-                operations.cell(i, 2)
-                    .date(new Date(row.created_at_timestamp * 1000)).style({ numberFormat: 'yyyy-mm-dd' });
-                operations.cell(i, 3)
-                    .number(row.manager_id);
-                operations.cell(i, 4)
-                    .string(row.manager);
-                operations.cell(i, 5)
-                    .string(row.card);
-                operations.cell(i, 6)
-                    .string('RUB');
-                operations.cell(i, 7)
-                    .number(row.amount);
-                operations.cell(i, 8)
-                    .number(row.fee);
-                operations.cell(i, 9)
-                    .number(row.user_id);
-                operations.cell(i, 10)
-                    .string(row.first_name);
-                operations.cell(i, 11)
-                    .string(row.last_name);
-                operations.cell(i, 12)
-                    .string(row.login);
-                operations.cell(i, 13)
-                    .string(row.email);
-            });
-
-            wb.write(`Stats${Date.now()}.xlsx`, res);
-        } catch (error) {
-            logger.error('[statisticController][getOperations]', error);
-            res.status(500).json({
-                name: error.name,
-                message: error.message,
-            });
-        }
-    })();
+const gettransactions = (req, res) => {
+  (async () => {
+    try {
+      const {user} = res.locals;
+      if (!user.isAdmin) throw new errors.PermissionDeniedError();
+      
+      // Get DB data
+      const data = await Promise.all([
+        db.getExchangeHistory()
+      ]);
+      
+      // Create a book
+      const wb = new xl.Workbook({
+        defaultFont: {
+          size: 12,
+          name: 'Calibri',
+        },
+        author: 'Narfex',
+      });
+      
+      const transactions = wb.addWorksheet('Transactions');
+      transactions.cell(1, 1).string('Type');
+      transactions.cell(1, 2).string('ID');
+      transactions.cell(1, 3).string('Address');
+      transactions.cell(1, 4).string('FromAmount');
+      transactions.cell(1, 5).string('FromCurrency');
+      transactions.cell(1, 6).string('ToAmout');
+      transactions.cell(1, 7).string('ToCurrency');
+      transactions.cell(1, 8).string('Fee');
+      transactions.cell(1, 9).string('FeeCurrency');
+      transactions.cell(1, 10).string('ReferReward');
+      transactions.cell(1, 11).string('Date');
+      transactions.cell(1, 12).string('Card');
+      transactions.cell(1, 13).string('Holder');
+      transactions.cell(1, 14).string('Bank');
+      transactions.cell(1, 15).string('ManagerID');
+      transactions.cell(1, 16).string('ManagerName');
+      transactions.cell(1, 17).string('Link');
+      
+      data[0].map((row, index) => {
+        const i = index + 2;
+        transactions.cell(i, 1).string(row.type);
+        transactions.cell(i, 2).number(row.request_id);
+        transactions.cell(i, 3).string(row.account_address);
+        transactions.cell(i, 4).number(row.source_amount);
+        transactions.cell(i, 5).string(row.source_currency);
+        transactions.cell(i, 6).number(row.target_amount);
+        transactions.cell(i, 7).string(row.target_currency);
+        transactions.cell(i, 8).number(row.commission);
+        transactions.cell(i, 9).string(row.commission_currency);
+        transactions.cell(i, 10).number(row.refer_reward);
+        transactions.cell(i, 11).date(new Date(row.timestamp * 1000));
+        transactions.cell(i, 12).string(row.card || '');
+        transactions.cell(i, 13).string(row.holder || '');
+        transactions.cell(i, 14).string(row.bank || '');
+        transactions.cell(i, 15).number(row.manager_id || 0);
+        transactions.cell(i, 16).string(`${row.manager_first || ''} ${row.manager_last || ''}`.trim());
+        transactions.cell(i, 17).string(`https://bscscan.com/tx/${row.tx_hash}`);
+      });
+      
+      wb.write(`Stats${Date.now()}.xlsx`, res);
+    } catch (error) {
+      logger.error('[statisticController][gettransactions]', error);
+      res.status(500).json({
+        name: error.name,
+        message: error.message,
+      });
+    }
+  })();
 };
 
 module.exports = {
-    getOperations,
+  gettransactions,
 };

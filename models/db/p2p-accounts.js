ab192af0364df211b5c3fc480a4d3eefbb941c37
Danil Sakhinov <ldevil2209@ya.ru>
2023-05-07 14:20:15 +0800

    Update

---
+++ models/db/p2p-accounts.js
@@ -0,0 +1,50 @@
+const _ = require('lodash');
+const logger = require('../../utils/logger');
+const db = require('../../services/mysql');
+const DataModel = require('./data-model');
+
+const model = new DataModel({
+  address: {
+    type: 'string',
+  },
+  isKYCVerified: {
+    field: 'isKYCVerified',
+    type: 'boolean',
+  },
+  isValidator: {
+    field: 'isValidator',
+    type: 'boolean',
+  },
+  name: {
+    field: 'name',
+    type: 'string',
+  },
+  telegram: {
+    field: 'telegram',
+    type: 'number',
+  },
+});
+
+const dataBaseName = 'p2p_accounts';
+
+const setKYCVerified = async address => {
+  try {
+    const query = `
+        INSERT INTO ${dataBaseName} (address, isKYCVerified)
+        VALUES (
+          '${address}',
+          1
+        )
+        ON DUPLICATE KEY
+        UPDATE
+        isKYCVerified=1;`;
+    return await db.query(query);
+  } catch (error) {
+    logger.error('[setKYCVerified]', error);
+    return null;
+  }
+};
+
+module.exports = {
+  setKYCVerified,
+};

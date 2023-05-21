7bdd4d9d41006285068086eece65f2d3c27bfa58
Danil Sakhinov <ldevil2209@ya.ru>
2023-05-21 17:36:02 +0800

    Turn off TON service

--- services/networks.js
+++ services/networks.js
@@ -1,5 +1,4 @@
 const web3Service = require('../services/web3');
-const tonService = require('../services/ton');
 
 /**
  * Get right network service
@@ -9,13 +8,10 @@ const tonService = require('../services/ton');
 const getService = network => {
     let service;
     switch (network) {
-        case 'TON':
-            service = tonService;
-            break;
         case 'BSC':
         case 'BEP20':
         default:
-            service = web3Service;
+            service = web3Service[network];
     }
     return service;
 };
